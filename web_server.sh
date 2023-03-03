23b007810c5623b46350db5ffc9c71953855ff63
Mikhail Kalyujniy <mih.kalyujniy@yandex.ru>
2023-03-03 15:55:06 +0300

    123

--- web_server.sh
+++ web_server.sh
@@ -5,8 +5,17 @@ hostname web-server
 yum install -y nginx
 systemctl enable nginx --now
 
-cat <<'EOT' > /etc/rsyslog.d/all.conf
+#Логи аудита должны также уходить на удаленную систему
+cat <<'EOT' > /etc/rsyslog.d/audit.conf
+$ModLoad imfile
+$InputFileName /var/log/audit/audit.log
+$InputFileTag tag_audit_log:
+$InputFileStateFile audit_log
+$InputFileSeverity info
+$InputFileFacility local6
+$InputRunFileMonitor
 *.* @@192.168.1.3:514
 EOT
+
 
 systemctl restart rsyslog
