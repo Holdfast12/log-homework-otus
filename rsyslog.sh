32571b0d8621bbaec10c6dee79c89d19dee12138
Mikhail Kalyujniy <mih.kalyujniy@yandex.ru>
2023-03-16 17:16:22 +0300

    123

--- rsyslog.sh
+++ rsyslog.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+/bin/cp -f /vagrant/log_rsyslog.conf /etc/rsyslog.conf
+systemctl restart rsyslog
--- web.sh
+++ web.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 dnf install -y nginx
+/bin/cp -f /vagrant/web_nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx --now
+
 
 # добавление правила аудита файла конфигурации nginx
 cat <<'EOT' >> /etc/audit/rules.d/nginx.rules
@@ -12,3 +14,23 @@ cat <<'EOT' >> /etc/audit/rules.d/nginx.rules
 
 EOT
 service auditd restart
+
+cat <<'EOT' > /etc/rsyslog.d/crit.conf
+*.crit @@rsyslog:514
+
+EOT
+
+cat <<'EOT' > /etc/rsyslog.d/audit.conf
+$ModLoad imfile
+$InputFileName /var/log/audit/audit.log
+$InputFileTag tag_audit_log:
+$InputFileStateFile audit_log
+$InputFileSeverity info
+$InputFileFacility local6
+$InputRunFileMonitor
+
+*.* @@rsyslog:514
+
+EOT
+
+systemctl restart rsyslog
